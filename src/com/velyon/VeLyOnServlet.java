33d306de5e34b6a0356926155cbed56eac357daa
Cyrille <bro.cy@outlook.fr>
2016-11-05 11:10:44 +0100

    Configuration en projet google-app-engine.

---
+++ src/com/velyon/VeLyOnServlet.java
@@ -0,0 +1,12 @@
+package com.velyon;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class VeLyOnServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
